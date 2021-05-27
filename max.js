bc5af18cd3ffae6df4ff278c0b20f087e471f495
maxq1seg <maximb00131516@gmail.com>
2021-05-27 08:13:02 +0300

    null js and html

--- max.js
+++
@@ -1,7 +0,0 @@
-console.log("max");
-console.log("i am serhet");
-try {
-  throw new Error("adkmasj");
-} catch (error) {
-  console.log(error.message);
-}
--- maxim.js
+++
@@ -1,2 +0,0 @@
-console.log("bernad");
-let a = 9;
